4fc306949459f7bfca8662ca4d9013c55764157e
highlaw00 <choiyool00@gmail.com>
2024-07-20 14:36:08 +0900

    Feat: 회원가입 API 구현
    - PasswordEncoder를 사용해 주어진 비밀번호를 해시합니다.
    - 해시된 비밀번호와 사용자 정보를 기준으로 회원가입을 진행합니다.

--- src/main/java/com/securehome/controller/SignUpController.java
+++ src/main/java/com/securehome/controller/SignUpController.java
@@ -5,6 +5,7 @@ import com.securehome.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class SignUpController {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/signup")
     public ResponseEntity<String> registerUser(@RequestBody Member member) {
         Member savedMember = null;
         ResponseEntity response = null;
         try {
+            String givenPassword = member.getPassword();
+            String encodedPassword = passwordEncoder.encode(givenPassword);
+            member.setPassword(encodedPassword);
             savedMember = memberRepository.save(member);
             if (savedMember.getId() > 0) {
                 response = ResponseEntity

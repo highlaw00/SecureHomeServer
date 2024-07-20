a828b1c9064ba804f2e827c5614d6c8e260f4694
highlaw00 <choiyool00@gmail.com>
2024-07-20 14:29:06 +0900

    Chore: 데이터베이스 초기화 DDL, DML 수정

--- init-scripts/fill-data.sql
+++ init-scripts/fill-data.sql
@@ -8,5 +8,5 @@ CREATE TABLE testdb.member
 );
 
 -- Insert some dummy data
-INSERT INTO member ( name, password, role ) VALUES("Test", "12345", "ADMIN");
-INSERT INTO member ( name, password, role ) VALUES("Data", "12345", "USER");
+INSERT INTO member ( name, password, role ) VALUES("admin", "$2a$10$wOAqXjtIMyCKR2oVy3mPlu4OYikW9iq9/novbYHfShxKPCESP44R.", "ADMIN");
+INSERT INTO member ( name, password, role ) VALUES("test", "$2a$10$bofKBprKo8m5OOa/nhzM8O7lx3e6.gmQfvkBM67dMT6OWC75bgWSa", "USER");
